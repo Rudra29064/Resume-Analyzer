fa679fe9d7425382e059b482062008d3f437c996
Rudra29064 <rudradhirendrakumarpatel@gmail.com>
2026-07-01 17:04:51 +0530

    fix: add Vercel URL to CORS whitelist

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://resumix29.vercel.app',
+  ],
   methods: ['GET', 'POST'],
 }));
 
